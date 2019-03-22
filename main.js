16d78c2eb9327335652ab137c57f1e3aa93930d2
kfunkin <32571927+kfunkin@users.noreply.github.com>
2019-03-22 21:54:10 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+anime({
+  targets: 'div.maratshead',
+  translateY: [
+    { value: 2, duration: 400, delay: 1100  },
+    { value: 0, duration: 400 },
+  ],
+  loop: true
+
+})
+anime({
+  targets: 'div.text1',
+  translateY: [
+    {value:1355, duration: 800, delay:900},
+    {value:0, duration:500, delay:3000}
+
+
+  ],
+opacity: [1,0],
+duration:60000,
+delay:900
+})
+
+
+anime({
+  targets:'div.aintmybagtxt',
+  translateX: [20],
+  opacity: [0,0.5],
+  duration:500,
+  delay: 1000,
+  easing: 'easeInOutSine'
+})
+
+anime({
+  targets: 'div.i-am-the-baltic',
+  opacity: [0,0.8],
+  duration: 2000,
+  delay: 6000,
+  easing: 'easeInOutSine'
+})
+
+anime({
+  targets: 'div.lady',
+  opacity: [0,0.8],
+  duration: 2000,
+  delay: 6000,
+  easing: 'easeInOutSine'
+})
+
+anime({
+  targets: 'div.pinkcircle',
+  opacity: [0,0.2],
+  duration: 5000,
+  delay: 8000
+})
+
+document.getElementById('#crash-leading').style.line.height = "1.6em";
